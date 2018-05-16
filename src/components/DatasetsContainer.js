07b4e6a4723c7b278f4ccefc0e716c845b50cc25
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-05-16 13:02:09 -0700

    added axios, data component

--- src/components/DatasetsContainer.js
+++ src/components/DatasetsContainer.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class DatasetsContainer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      datasets: []
+    }
+  }
   render() {
     return (
       <div className="DatasetsContainer">
