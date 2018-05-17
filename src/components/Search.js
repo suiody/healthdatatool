913ef0a7ed7181a464d72b56045df434e563534b
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-05-17 14:40:28 -0700

    starting to work on graph component

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,7 +10,9 @@ class Search extends Component {
    this.state = {
      countries: [],
      years: [],
-     indicators: []
+     indicators: [],
+     characteristics: [],
+     arrData: {}
    }
  }
 
@@ -41,14 +43,57 @@ class Search extends Component {
  }
 
  getIndicators(){
-      axios.get('https://api.dhsprogram.com/rest/dhs/indicators')
-      .then(response => {
-        console.log(response.data.Data[0])
-        this.setState({
-          indicators: response.data.Data
-        });
+    axios.get('https://api.dhsprogram.com/rest/dhs/indicators')
+    .then(response => {
+      console.log(response.data.Data[0])
+      this.setState({
+        indicators: response.data.Data
       });
+    });
  }
+
+
+// This should be called onChange, when the user makes a selection from drop down
+getCharacteristics(){
+    axios.get('https://api.dhsprogram.com/rest/dhs')
+    .then(response => {
+      this.setState({
+        characteristics: response.data.Data
+      });
+    });
+  }
+  // not every indicator will have a characteristic category, so if not, create it.
+  let characteristics = this.state.characteristics;
+  let arrData = {};
+
+ this.state.characteristics.map((value, index) => {
+   if(!arrData[value.CharacteristicCategory]) {
+       arrData[value.CharacteristicCategory] = {};
+       arrData[value.CharacteristicCategory][value.CharacteristicLabel] = {};
+       arrData[value.CharacteristicCategory][value.CharacteristicLabel][value.ByVariableLabel] = value.Value;
+   }
+   else if(!arrData[value.CharacteristicCategory][value.CharacteristicLabel])
+   {
+       arrData[value.CharacteristicCategory][value.CharacteristicLabel] = {};
+       arrData[value.CharacteristicCategory][value.CharacteristicLabel][value.ByVariableLabel] = value.Value;
+   }
+   else if(!arrData[value.CharacteristicCategory][value.CharacteristicLabel][value.ByVariableLabel])
+   {
+       if(value.ByVariableLabel.length > 0)
+       {
+           arrData[value.CharacteristicCategory][value.CharacteristicLabel][value.ByVariableLabel] = value.Value;
+       }
+   }
+   else
+   {
+       if(value.ByVariableLabel.length > 0)
+       {
+           arrData[value.CharacteristicCategory][value.CharacteristicLabel][value.ByVariableLabel] = value.Value;
+       }
+   }
+ })
+}
+
 
   render(){
     let countries = this.state.countries;
@@ -65,17 +110,22 @@ class Search extends Component {
   let indicatorItems = indicators.map((ind) =>
     <option key={ind.IndicatorId}>{ind.Label}</option>
 );
+
+
     return (
    <div>
-           <select>
-              {countryItems}
-           </select>
-           <select>
-              {yearItems}
-           </select>
-           <select>
-              {indicatorItems}
-           </select>
+       <p>Countries: </p>
+       <select className="dropDown">
+          {countryItems}
+       </select>
+       <p>Survey Years: </p>
+       <select className="dropDown">
+          {yearItems}
+       </select>
+       <p>Indicators: </p>
+       <select className="dropDown">
+          {indicatorItems}
+       </select>
        </div>
 
     );
