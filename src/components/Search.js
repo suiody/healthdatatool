80362120dc1ccc778619a62e7358d142adb2db7d
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-05-17 12:46:59 -0700

    original fetch I was working on...

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import axios from 'axios';
-// import $ from 'jquery';
+
 
 class Search extends Component {
 
@@ -12,24 +12,16 @@ class Search extends Component {
    }
  }
 
-componentDidMount() {
-
-// This is wrong...it's being edited!
-  var initialCountries = [];
-  // fetch the country data, it will be returned as json by default
-  axios.get('https://api.dhsprogram.com/rest/dhs/countries')
-  .then(response => {
-    console.log(response.data)
-     return response.data.Data;
-   }).then(data => {
-    initialCountries = data.results.map((country, index) =>{
-      return country
-    });
-    this.setState({
-      countries: initialCountries
-    });
-  });
-}
+ componentDidMount() {
+   // fetch the country data, it will be returned as json by default
+   axios.get('https://api.dhsprogram.com/rest/dhs/countries')
+   .then(response => {
+     console.log(response.data)
+     this.setState({
+       countries: response.data
+     });
+   });
+ }
 
 
   render(){
