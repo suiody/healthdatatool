fb93ef3c014152ce85f3af8a79c4da136b81bb38
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-05-23 19:14:46 -0700

    working on refactoring api calls

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,9 +19,10 @@ class Search extends Component {
      arrData: {}, // holds selected query results
      characteristics: [], // holds characteristic groups for the selected indicator,
      selectedCharacteristic: [], // selected characteristic
-     CountryId: '', // keeps track of the countryId for DHS query
-     SurveyId: '', // keeps track of surveyId for DHS query
-     IndicatorId: '' // keeps track of indicatorId for DHS query
+     strCountry: '', // keeps track of the countryId for DHS query
+     strSurveyYear: '', // keeps track of surveyId for DHS query
+     strIndicator: '', // keeps track of indicatorId for DHS query,
+     strCharGroup: '' // keeps track of current characteristic group to map
    }
    this.keyCount = 0;
    this.getKey = this.getKey.bind(this);
@@ -33,7 +34,7 @@ class Search extends Component {
  }
 
  getKey(){
-    return this.keyCount++;
+  return this.keyCount++;
 }
 
 // query rails API for countries
@@ -50,18 +51,31 @@ class Search extends Component {
     console.log(err);
   }
  }
+
  // store the selected country from dropdown menu in state
   handleCountry(e){
-    this.setState({selectedCountry: e.target.value});
+    this.setState({ selectedCountry: e.target.value });
     var selectedCountry = e.target.value;
     var result = this.state.countries.filter((co) =>
       co.CountryName === selectedCountry
    );
     var strCountry = result[0].DHS_CountryCode.toString();
-   // need the CountryId
+    this.setState({ strCountry: strCountry })
     this.getSurveyYears(strCountry);
    }
 
+// need to refactor considering cases where there is only one survey year, doesn't work yet...
+handleYear(e){
+   this.setState({selectedYear: e.target.value});
+   var selectedYear = e.target.value;
+   var result = this.state.years.filter((yr) =>
+     yr.SurveyYear === selectedYear
+  );
+  // need to refactor...grab value
+   var strSurveyYear = result[0].SurveyYear;
+   this.setState({ strSurveyYear: strSurveyYear })
+   this.getIndicators(this.state.strCountry, this.state.strSurveyYear);
+}
 getSurveyYears(strCountry){
     var gAPIDomain = "https://api.dhsprogram.com/rest/dhs/"
     var apiURL =  gAPIDomain + "surveys/" + strCountry + "?surveyType=DHS";
@@ -75,8 +89,8 @@ getSurveyYears(strCountry){
     });
 }
 
- // have to alter this, needs to be dynamic, e.g. indicators populated after user selects country
- async getIndicators(){
+// refactor this
+ async getIndicators(strCountry, strSurveyYear){
    var gAPIDomain = "https://api.dhsprogram.com/rest/dhs"
    var strCountry = this.state.selectedCountry;
    var strSurveyYear = this.state.selectedYear;
@@ -99,14 +113,11 @@ getSurveyYears(strCountry){
   }
  }
 
- handleYear(e){
-   this.setState({selectedYear: e.target.value});
- }
-
 // store the selected indicator from dropdown menu in state
  handleIndicator(e){
    this.setState({IndicatorId: e.target.value});
  }
+
 
  handleCharacteristics(e){
   this.setState({selectedCharacteristic: e.target.value});
@@ -143,7 +154,6 @@ getSurveyYears(strCountry){
  }
 
 // populate the characteristic groups
-// need to refactor so this does not get called until after indicator is selected...
  getCharacteristicGroups(){
    var arrData = this.state.arrData;
    [arrData].forEach(function(value) {
@@ -166,39 +176,42 @@ getSurveyYears(strCountry){
   // populate the characteristics menu from the selected indicator
     var listCharGroups = Object.keys(arrData);
     this.setState({characteristics: listCharGroups});
-    this.populateCharacteristics();
-}
-
-populateCharacteristics(){
-  let characteristics = this.state.characteristics;
-  let charItems = characteristics.map((char) =>
-    <option key={this.getKey()}>{char}</option>
- );
 }
 
   render(){
     let countries = this.state.countries;
-    let countryItems = countries.map((country) =>
-      <option key={country.CountryId}>{country.CountryName}</option>
-  );
-
-  //   let years = this.state.years;
-  //   let yearItems = years.map((year) =>
-  //     <option key={year.SurveyId}>{year.SurveyYearLabel}</option>
-  // );
-  //
-  //   let indicators = this.state.indicators;
-  //   let indicatorItems = indicators.map((ind) =>
-  //     <option key={ind.IndicatorId}>{ind.Definition}</option>
-  // );
+    let years = this.state.years;
+    let indicators = this.state.indicators;
+    let characteristics = this.state.characteristics;
 
     return (
    <div className="container-fluid">
        <p className="searchTitles">Countries: </p>
        <select className="dropDown" onChange={(e) => this.handleCountry(e)}>
-          {countryItems}
+        {
+          this.state.countries.map((country) =>
+           <option key={country.CountryId}>{country.CountryName}</option>
+          )
+       }
+     );
        </select>
-
+       <p className="searchTitles">Survey Years: </p>
+      <select className="dropDown" onChange={(e) => this.handleYear(e)}>
+         {
+            this.state.years.map((year) =>
+              <option key={year.SurveyId}>{year.SurveyYearLabel}</option>
+            )
+         }
+      </select>
+      <p className="searchTitles">Indicators: </p>
+      <select className="dropDown" onChange={(e) => this.handleIndicator(e)}>
+        {
+          this.state.indicators.map((ind) =>
+            <option key={ind.IndicatorId}>{ind.Label}</option>
+          )
+        }
+      </select>
+      <p className="searchTitles">Characteristics: </p>
 
        </div>
     );
