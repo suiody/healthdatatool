a9cc136e3d9a2f8814c1e18f1118bdb30f2d4226
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-06-17 17:41:18 -0700

    updates to text of alert messages, and address bug with menu load in archives

--- src/components/Archives.js
+++ src/components/Archives.js
@@ -60,7 +60,7 @@ async getInfantMortality(){
    }
  } catch(err){
    console.log(err);
-   window.alert("There was a problem connecting to the archives. Please try again later, or search one of the other databases.");
+   window.alert("There was a problem connecting to the infant mortality archives. Please try again later, or search one of the other databases.");
    window.location = "/";
  }
  this.getChildMortality();
@@ -79,7 +79,7 @@ async getChildMortality(){
    }
  } catch(err){
    console.log(err);
-   window.alert("There was a problem connecting to the archives. Please try again later, or search one of the other databases.")
+   window.alert("There was a problem connecting to the child mortality archives. Please try again later, or search one of the other databases.")
    window.location = "/";
  }
 }
@@ -95,15 +95,17 @@ populateData(strIndicator){
       var infantData = this.state.infantData;
         if (infantData.length >= 1) {
           var arrDataInf = this.state.arrDataInf;
-          infantData.forEach(function(value,index){
-             hash = {};
-              hash["CountryName"] = value.CountryName;
-              hash["SurveyYear"] = value.SurveyYear;
-              hash["Indicator"] = value.Indicator;
-              hash["CharacteristicLabel"] = value.CharacteristicLabel;
-              hash["Value"] = value.Value;
-              arrDataInf.push(hash);
-          });
+          if (arrDataInf.length >= 1) {
+            infantData.forEach(function(value,index){
+               hash = {};
+                hash["CountryName"] = value.CountryName;
+                hash["SurveyYear"] = value.SurveyYear;
+                hash["Indicator"] = value.Indicator;
+                hash["CharacteristicLabel"] = value.CharacteristicLabel;
+                hash["Value"] = value.Value;
+                arrDataInf.push(hash);
+            });
+          }
           // array to hold country values, contains duplicates
            tmp = [];
            arrDataInf.forEach(function(value,index){
@@ -117,6 +119,7 @@ populateData(strIndicator){
     var childData = this.state.childData;
       if (childData.length >= 1) {
         var arrDataCh = this.state.arrDataCh;
+        if (arrDataCh.length >= 1) {
         childData.forEach(function(value,index){
             hash = {};
             hash["CountryName"] = value.CountryName;
@@ -126,7 +129,7 @@ populateData(strIndicator){
             hash["Value"] = value.Value;
             arrDataCh.push(hash);
         });
-
+      }
          tmp = [];
          arrDataCh.forEach(function(value,index){
            tmp.push(value.CountryName);
